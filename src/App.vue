8fa292e6cb8f0e3ab2943f8e38d9705b5a0e89a5
yeah-right <claar.zack@gmail.com>
2019-06-19 22:56:52 -0600

    Starting on express backend

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 offset-md-3">
+          <ChatRoom />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatRoom from './components/ChatRoom.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ChatRoom
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/ChatRoom.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="card mt-3">
+    <div class="card-body">
+      <div class="card-title">
+        <h3>Chat Group</h3>
+          <hr>
+      </div>
+      <div class="card-body">
+        <div class="messages">
+                  
+        </div>
+      </div>
+    </div>
+    <div class="card-footer">
+      <form @submit.prevent="sendMsg">
+        <div class="form-group">
+          <label for="user">usR: </label>
+          <input type="text" v-model="user" class="form-control">
+        </div>
+        <div class="form-group pb-3">
+          <label for="message">msG: </label>
+          <input type="text" v-model="message" class="form-control">
+        </div>
+        <button type="submit" class="btn btn-success">post reQuest</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ChatRoom',
+  props: {
+    messge: String,
+    user: String,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+} */
+</style>
