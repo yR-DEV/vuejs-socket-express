168213d3b5315ab7964c1bc1d23fba15d2e4fb23
yeah-right <claar.zack@gmail.com>
2019-06-19 23:03:11 -0600

    minimal express backend served with nodemon created, back to the front end now

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+server = app.listen(3001, function() {
+    console.log('server is listening for changes\n',
+    'on TEH p00rt: 3001');
+});
+
+const io = require('socket.io')(server);
+
+io.on('connection', function(socket) {
+    console.log(socket.id);   
+});
+
+
